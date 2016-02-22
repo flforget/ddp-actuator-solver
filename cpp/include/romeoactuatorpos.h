10b7b125d0ca24579fb26c7f32a5101c1b3df75e
flforget <florent.forget1@gmail.com>
2016-02-22 11:09:31 +0100

    [major]Add cmake file to find qpOASES [minor]new executable fo test QPBox

---
+++ cpp/include/romeoactuatorpos.h
@@ -0,0 +1,64 @@
+#ifndef ROMEOACTUATORPOS_H
+#define ROMEOACTUATORPOS_H
+
+#include "config.h"
+
+#include "dynamicmodel.h"
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+class RomeoActuatorPos : public DynamicModel
+{
+public:
+    RomeoActuatorPos(double& mydt);
+private:
+protected:
+
+    // attributes //
+public:
+private:
+    double dt;
+    //static const unsigned int stateNb=4;
+    //static const unsigned int commandNb=1;
+public:
+    static const double k;
+    static const double R;
+    static const double Jm;
+    static const double Jl;
+    static const double fvm;
+    static const double Cf0;
+    static const double a;
+
+private:
+    stateVec_t Xreal;
+    stateMat_t Id;
+    stateMat_t A;
+    stateMat_t Ad;
+    stateR_commandC_t B;
+    stateR_commandC_t Bd;
+    double A13atan;
+    double A33atan;
+    stateMat_t fxBase;
+    stateR_commandC_t fuBase;
+
+    stateMat_t QxxCont;
+    commandMat_t QuuCont;
+    commandR_stateC_t QuxCont;
+
+protected:
+    // methods //
+public:
+    stateVec_t computeNextState(double& dt, const stateVec_t& X,const stateVec_t& Xdes, const commandVec_t &U);
+    void computeAllModelDeriv(double& dt, const stateVec_t& X,const stateVec_t& Xdes, const commandVec_t &U);
+    stateMat_t computeTensorContxx(const stateVec_t& nextVx);
+    commandMat_t computeTensorContuu(const stateVec_t& nextVx);
+    commandR_stateC_t computeTensorContux(const stateVec_t& nextVx);
+private:
+protected:
+        // accessors //
+public:
+
+};
+
+#endif // ROMEOSIMPLEACTUATOR_H
---
+++ cpp/test/testModel.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+
+#include "config.h"
+
+#include "ilqrsolver.h"
+#include "romeosimpleactuator.h"
+#include "romeolinearactuator.h"
+#include "romeoactuatorpos.h"
+#include "costfunctionromeoactuator.h"
+
+#include <time.h>
+#include <sys/time.h>
+
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    struct timeval tbegin,tend;
+    double texec=0.0;
+    stateVec_t xinit,xDes,x;
+    commandVec_t u;
+
+    u << 1.0;
+
+    xinit << -3.0,0.0,0.0,0.0;
+    xDes << 0.0,0.0,0.0,0.0;
+
+    x = xinit;
+
+    unsigned int T = 50;
+    double dt=1e-4;
+    unsigned int iterMax = 100;
+    double stopCrit = 1e-5;
+    stateVec_t* xList;
+    commandVec_t* uList;
+    ILQRSolver::traj lastTraj;
+
+    RomeoSimpleActuator romeoActuatorModel(dt);
+    RomeoActuatorPos romeoActuator(dt);
+    RomeoLinearActuator romeoLinearModel(dt);
+    CostFunctionRomeoActuator costRomeoActuator;
+
+    /*cout << romeoActuatorModel.getCommandNb() << endl;
+
+    for(unsigned int i=0;i<100;i++)
+    {
+        x = romeoActuator.computeNextState(dt,x,xDes,u);
+        cout << x << endl<<"-"<<endl;
+    }*/
+
+    ILQRSolver testSolverRomeoActuator(romeoActuatorModel,costRomeoActuator,1);
+    testSolverRomeoActuator.FirstInitSolver(xinit,xDes,T,dt,iterMax,stopCrit);
+
+
+    int N = 1;
+    gettimeofday(&tbegin,NULL);
+    for(int i=0;i<N;i++) testSolverRomeoActuator.solveTrajectory();
+    gettimeofday(&tend,NULL);
+
+    lastTraj = testSolverRomeoActuator.getLastSolvedTrajectory();
+    xList = lastTraj.xList;
+    uList = lastTraj.uList;
+    unsigned int iter = lastTraj.iter;
+
+    texec=((double)(1000*(tend.tv_sec-tbegin.tv_sec)+((tend.tv_usec-tbegin.tv_usec)/1000)))/1000.;
+    texec /= N;
+
+    cout << endl;
+    cout << "temps d'execution total du solveur ";
+    cout << texec << endl;
+    cout << "temps d'execution par pas de temps ";
+    cout << texec/T << endl;
+    cout << "Nombre d'itérations : " << iter << endl;
+
+
+
+
+
+    ofstream fichier("results.csv",ios::out | ios::trunc);
+    if(fichier)
+    {
+        fichier << "tau,tauDot,q,qDot,u" << endl;
+        for(int i=0;i<T;i++) fichier << xList[i](0,0) << "," << xList[i](1,0) << "," << xList[i](2,0) << "," << xList[i](3,0) << "," << uList[i](0,0) << endl;
+        fichier << xList[T](0,0) << "," << xList[T](1,0) << "," << xList[T](2,0) << "," << xList[T](3,0) << "," << 0.0 << endl;
+        fichier.close();
+    }
+    else
+        cerr << "erreur ouverte fichier" << endl;
+    return 0;
+
+}
